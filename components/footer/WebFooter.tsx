5ea00f1e0b4655451fb40c764b3c53fa3843db18
kawaffe <187086553+kawaffee@users.noreply.github.com>
2024-12-15 21:24:01 +0200

    footer must stay scrolled

--- components/footer/WebFooter.tsx
+++ components/footer/WebFooter.tsx
@@ -1,28 +1,32 @@
 import type { JSX } from "preact/jsx-runtime";
 
-export default function WebFooter(): JSX.Element {
-    return (
-        <footer class="footer-container">
-            <div class="footer-text">
-                <span>&copy; {new Date().getFullYear()} Mooshieblob</span>
-                <a
-                    id="ipv6"
-                    href="https://iplocation.io/ipv6-compatibility-checker?url=referer"
-                >
-                    <img
-                        src="https://iplocation.io/images/ipv6-non-validated.svg"
-                        alt="ipv6 ready"
-                        title="ipv6 ready"
-                        style={{
-                            border: 0,
-                            padding: 0,
-                            width: "36px",
-                            height: "18px",
-                            marginLeft: "10px",
-                        }}
-                    />
-                </a>
-            </div>
-        </footer>
-    );
+interface WebFooterProps {
+	class?: string;
+}
+
+export default function WebFooter(props: WebFooterProps): JSX.Element {
+	return (
+		<footer class={`footer-container ${props.class ?? ""}`}>
+			<div class="footer-text">
+				<span>&copy; {new Date().getFullYear()} Mooshieblob</span>
+				<a
+					id="ipv6"
+					href="https://iplocation.io/ipv6-compatibility-checker?url=referer"
+				>
+					<img
+						src="https://iplocation.io/images/ipv6-non-validated.svg"
+						alt="ipv6 ready"
+						title="ipv6 ready"
+						style={{
+							border: 0,
+							padding: 0,
+							width: "36px",
+							height: "18px",
+							marginLeft: "10px",
+						}}
+					/>
+				</a>
+			</div>
+		</footer>
+	);
 }
