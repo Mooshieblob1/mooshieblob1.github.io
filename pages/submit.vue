d9f7a42711b1f6791d8377d3fae712dd94e8459b
Mooshieblob1 <blob@mooshieblob.com>
2025-08-06 06:11:55 +0800

    Updates page heading to clarify submission purpose
    Changes the heading text from "Submit Your Ideas" to "Submit Your Image requests" to better reflect the specific type of content users should submit through the form.

--- pages/submit.vue
+++ pages/submit.vue
@@ -11,7 +11,7 @@
         />
       </NuxtLink>
       <h1 class="mb-4 text-center text-2xl font-bold text-yellow-400">
-        Submit Your Ideas
+        Submit Your Image requests.
       </h1>
       <form
         action="https://formsubmit.co/blob@mooshieblob.com"
