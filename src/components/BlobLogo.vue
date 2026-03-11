d25b95f3af2125cc2c481cdaa5df7bfa4b18cbb3
Mooshieblob1 <blob@mooshieblob.com>
2026-03-12 02:12:00 +0800

    refactor: Remove unused visibility logic and simplify BlobLogo component

--- src/components/BlobLogo.vue
+++ src/components/BlobLogo.vue
@@ -3,27 +3,6 @@
     id="main-logo"
     src="/assets/images/bloblogo.webp"
     alt=""
-    class="social-logo transition-opacity duration-500"
-    :class="{ 'opacity-0': !isVisible, 'opacity-100': isVisible }"
+    class="social-logo"
   />
 </template>
-
-<script setup lang="ts">
-import { ref, onMounted, watch } from 'vue';
-import { splashState } from '../stores/splash';
-
-const isVisible = ref(splashState.isAnimationDone);
-
-onMounted(() => {
-  // Ensure visibility on mount (handles View Transitions re-hydration)
-  if (sessionStorage.getItem('splash-shown')) {
-    isVisible.value = true;
-    splashState.isAnimationDone = true;
-  }
-});
-
-// React to splash completing
-watch(() => splashState.isAnimationDone, (val) => {
-  if (val) isVisible.value = true;
-});
-</script>
